39b0289edf6886f37107881be5380f3c9cb87dfa
Simonas Sankauskas <simonas.sank@gmail.com>
2017-05-02 10:59:45 +0300

    Grades Recycler View

---
+++ app/src/main/java/lt/welovedotnot/ktu_ais_app/views/components/GradesRecyclerView.kt
@@ -0,0 +1,39 @@
+package lt.welovedotnot.ktu_ais_app.views.components
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import lt.welovedotnot.ktu_ais_app.api.models.WeekModel
+import lt.welovedotnot.ktu_ais_app.views.activities.adapters.WeekListAdapter
+import android.support.v7.widget.LinearLayoutManager
+
+
+
+/**
+ * Created by simonas on 5/2/17.
+ */
+
+class GradesRecyclerView : RecyclerView {
+    val adapter = WeekListAdapter()
+
+    constructor(context: Context)
+            : super(context) { init() }
+
+    constructor(context: Context, attrs: AttributeSet)
+            : super(context, attrs) { init() }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr) { init() }
+
+    private fun init() {
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        setLayoutManager(layoutManager)
+        setAdapter(adapter)
+    }
+
+    fun setModels(modelList: MutableList<WeekModel>) {
+        adapter.list.clear()
+        adapter.list.addAll(modelList)
+        adapter.notifyDataSetChanged()
+    }
+}
