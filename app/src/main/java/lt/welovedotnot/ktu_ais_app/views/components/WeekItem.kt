f728566fce8e56105553701fe9b04e1f7cd3d702
Simonas Sankauskas <simonas.sank@gmail.com>
2017-05-02 11:00:04 +0300

    WeekItem Margins

--- app/src/main/java/lt/welovedotnot/ktu_ais_app/views/components/WeekItem.kt
+++ app/src/main/java/lt/welovedotnot/ktu_ais_app/views/components/WeekItem.kt
@@ -4,9 +4,17 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
+import com.mcxiaoke.koi.ext.dpToPx
 import kotlinx.android.synthetic.main.week_item.view.*
 import lt.welovedotnot.ktu_ais_app.R
 import lt.welovedotnot.ktu_ais_app.api.models.WeekModel
+import android.R.attr.right
+import android.R.attr.left
+import android.support.v4.content.ContextCompat
+import android.view.ViewGroup
+import android.widget.LinearLayout
+
+
 
 /**
  * Created by simonas on 5/1/17.
@@ -23,14 +31,40 @@ class WeekItem: RelativeLayout {
         inflate(context, R.layout.week_item, this)
     }
 
-    fun setModel(model: WeekModel) {
+    fun setModel(model: WeekModel): WeekItem {
         weekNumber.text = model.savaitesNr
         gradesLinearLayout.removeAllViews()
 
-        model.grades?.forEach { item ->
+        model.grades?.forEachIndexed { index, gradeModel ->
             val gradeView = GradeItem(context)
-            gradeView.setModel(item)
+            gradeView.setModel(gradeModel)
             gradesLinearLayout.addView(gradeView)
+
+            if (index < model?.grades.size-1) {
+                gradeView.setMargin(bottom = 12.dpToPx())
+            }
         }
+
+        return this
     }
+
+    fun ViewGroup.setMargin(left: Int = -1, top: Int = -1, right: Int = -1, bottom: Int = -1) {
+        val params =  this.layoutParams as MarginLayoutParams
+        if (left != -1) {
+            params.leftMargin = left
+        }
+        if (top != -1) {
+            params.topMargin = top
+        }
+        if (right != -1) {
+            params.rightMargin = right
+        }
+        if (bottom != -1) {
+            params.bottomMargin = bottom
+        }
+
+        this.layoutParams = params
+        this.requestLayout()
+    }
+
 }
