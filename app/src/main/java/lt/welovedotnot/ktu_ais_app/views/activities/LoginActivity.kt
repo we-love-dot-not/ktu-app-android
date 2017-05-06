d359143695821573bb2c4036a98200aa71f6fe48
Kestutis <kiesiusp@gmail.com>
2017-05-06 19:55:24 +0300

    Nepasileido bet teoriskai turi paeit

--- app/src/main/java/lt/welovedotnot/ktu_ais_app/views/activities/LoginActivity.kt
+++ app/src/main/java/lt/welovedotnot/ktu_ais_app/views/activities/LoginActivity.kt
@@ -5,15 +5,36 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.rengwuxian.materialedittext.MaterialEditText
+import io.realm.RealmList
 import kotlinx.android.synthetic.main.activity_main.*
 import lt.welovedotnot.ktu_ais_app.R
+import lt.welovedotnot.ktu_ais_app.api.models.GradeModel
+import lt.welovedotnot.ktu_ais_app.api.models.WeekModel
 import lt.welovedotnot.ktu_ais_app.db.User
+import lt.welovedotnot.ktu_ais_app.views.notifications.KTUNotification
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gradeUpdate = KTUNotification()
+
+        val gradeModel = GradeModel()
+        gradeModel.name = "Diskrečiosios struktūros"
+        gradeModel.id = "P170B008"
+        gradeModel.type = "Inžinerinis projektas"
+        gradeModel.typeId = "IR"
+        gradeModel.profestor = "Koordinuojantysis dėstytojas doc. M. Patašius"
+        gradeModel.mark = "09"
+
+        val weekNum: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+        val gradesList: RealmList<GradeModel> = RealmList(gradeModel)
+        val week = WeekModel()
+        week.weekNumbers = weekNum
+        week.grades = gradesList
+        gradeUpdate.sendUpcomingTestNotification(week, this)
 
         loginBtn.setOnClickListener {
 
---
+++ app/src/main/java/lt/welovedotnot/ktu_ais_app/views/notifications/KTUNotification.java
@@ -0,0 +1,62 @@
+package lt.welovedotnot.ktu_ais_app.views.notifications;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import lt.welovedotnot.ktu_ais_app.R;
+import lt.welovedotnot.ktu_ais_app.api.models.GradeModel;
+import lt.welovedotnot.ktu_ais_app.api.models.WeekModel;
+import lt.welovedotnot.ktu_ais_app.views.activities.LoginActivity;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+/**
+ * Created by Vartotojas on 2017-05-06.
+ */
+
+public class KTUNotification {
+
+    public void sendGradeNotification(GradeModel model, Context context) {
+        String title = "Gavai " + model.getMark();
+        String text = model.getName() + ", " + model.getTypeId();
+        sendNotification(title, text, context);
+    }
+
+    public void sendUpcomingTestNotification(WeekModel model, Context context) {
+        String title = "Kitos savaitės atsiskaitymai";
+        String text = "a\na\na\n";
+        sendNotification(title, text, context);
+    }
+
+    private void sendNotification(String title, String text, Context context) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_priority_high_black_48dp)
+                        .setContentTitle(title)
+                        .setContentText(text);
+        Intent resultIntent = new Intent(context, LoginActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(LoginActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+}
