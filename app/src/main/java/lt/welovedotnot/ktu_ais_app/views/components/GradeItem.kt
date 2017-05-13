2baeac78b09471c6e4ed9f24ea4df3e7ebd498a4
Simonas Sankauskas <simonas.sank@gmail.com>
2017-05-14 00:59:01 +0300

    Babushka Grade View

--- app/src/main/java/lt/welovedotnot/ktu_ais_app/views/components/GradeItem.kt
+++ app/src/main/java/lt/welovedotnot/ktu_ais_app/views/components/GradeItem.kt
@@ -1,11 +1,16 @@
 package lt.welovedotnot.ktu_ais_app.views.components
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.grade_item.view.*
 import lt.welovedotnot.ktu_ais_app.R
 import lt.welovedotnot.ktu_ais_app.models.GradeModel
+import android.support.v4.content.ContextCompat.getColor
+import babushkatext.BabushkaText
+
+
 
 /**
  * Created by simonas on 5/1/17.
@@ -25,6 +30,32 @@ class GradeItem: RelativeLayout {
     fun setModel(model: GradeModel) {
         className.text = model.name
         gradeType.text = model.type
-        grade.text = model.mark?.split(";")?.joinToString(" ")
+        val gradeList = model.mark?.split(";")
+        val textList = mutableListOf<String>()
+        var lastMarkIndex = -1
+        gradeList?.forEachIndexed { index, gradePiece ->
+            var gradePieceMut = gradePiece
+            if (index < gradeList.size-1) {
+                gradePieceMut += " "
+            }
+            if (gradePiece != GradeModel.EMPTY_MARK) {
+                lastMarkIndex = index
+            }
+            textList.add(gradePieceMut)
+        }
+
+        textList.forEachIndexed { index, text ->
+            val primColor = getColor(context, android.R.color.primary_text_light)
+            val secColor = getColor(context, android.R.color.secondary_text_dark)
+            var color = secColor
+            if (index == lastMarkIndex) {
+                color = primColor
+            }
+
+            gradeText.addPiece(BabushkaText.Piece.Builder(text)
+                    .textColor(color)
+                    .build())
+        }
+        gradeText.display()
     }
 }
