4dea775829114a0b4530831a1e05ebe5b6ddbb11
Simonas Sankauskas <simonas.sank@gmail.com>
2017-05-14 09:39:42 +0300

    Login Unit Test fix

--- app/src/test/java/lt/welovedotnot/ktu_ais_app/LoginUnitTest.kt
+++ app/src/test/java/lt/welovedotnot/ktu_ais_app/LoginUnitTest.kt
@@ -1,8 +1,12 @@
 package lt.welovedotnot.ktu_ais_app
 
+import com.mcxiaoke.koi.log.logi
+import org.junit.Test
+
 import lt.welovedotnot.ktu_ais_app.api.Api
 import lt.welovedotnot.ktu_ais_app.models.LoginRequest
-import org.junit.Test
+import lt.welovedotnot.ktu_ais_app.models.UserModel
+import org.junit.Assert.*
 
 import java.util.concurrent.CountDownLatch
 
@@ -15,22 +19,20 @@ class LoginUnitTest {
     @Test
     @Throws(Exception::class)
     fun login_isCorrect() {
+        var loginResponse: UserModel? = null
         val lock = CountDownLatch(1)
         val loginRequest = LoginRequest()
         loginRequest.username = "test_username"
         loginRequest.password = "test_password"
 
-        Api.login(loginRequest) { loginResponse ->
-            if (loginResponse != null) {
-                assert(loginResponse.cookie?.isEmpty()!!)
-
-            } else {
-                assert(false)
-            }
-
+        Api.login(loginRequest) {
+            loginResponse = it
             lock.countDown()
         }
 
         lock.await()
+
+        assertNotNull("Bad creds", loginResponse)
+        assertNotEquals("Cookie is empty.", loginResponse!!.cookie!!.isEmpty(), "")
     }
 }
